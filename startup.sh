6a94a8123957f486356f3d7a0c43387a0c955f5d
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-09-19 09:09:00 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+curl 'https://raw.githubusercontent.com/pingme998/booy-st/main/supervisord.conf' >/etc/supervisor/supervisord.conf
 if [ -n "$VNC_PASSWORD" ]; then
     echo -n "$VNC_PASSWORD" > /.password1
     x11vnc -storepasswd $(cat /.password1) /.password2
